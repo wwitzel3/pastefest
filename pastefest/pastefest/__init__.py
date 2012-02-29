562bdf7c87da00476b161dc49e6c0566f12a886d
Wayne Witzel III <wwitzel3@gmail.com>
2012-02-28 20:31:27 -0500

    fixups and prod changes

--- pastefest/pastefest/__init__.py
+++ pastefest/pastefest/__init__.py
@@ -11,8 +11,8 @@ def main(global_config, **settings):
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
     config.add_route('index', '/')
-    config.add_route('js', '/{id}.js')
-    config.add_route('view', '/{id}')
+    config.add_route('js', '/paste/{id}.js')
+    config.add_route('view', '/paste/{id}')
     config.scan()
     return config.make_wsgi_app()
 
