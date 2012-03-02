94e96376eb02b446b70222ec35c883df138dbfc8
Wayne Witzel III <wwitzel3@gmail.com>
2012-03-01 22:41:53 -0500

    Production.ini fixups for heroku

---
+++ README.txt
@@ -0,0 +1,3 @@
+Make sure you set your local environment variables:
+
+HEROKU_SHARED_POSTGRESQL_IVORY_URL = postges://user:pass@local:port/db
--- pastefest/pastefest/__init__.py
+++ pastefest/pastefest/__init__.py
@@ -6,13 +6,20 @@ from .models import DBSession
 def main(global_config, **settings):
     """ This function returns a Pyramid WSGI application.
     """
+    settings['sqlalchemy.url'] = os.environ['HEROKU_SHARED_POSTGRESQL_IVORY_URL']
+    settings['sqlalchemy.pool_size'] = 20
+    settings['sqlalchemy.max_overflow'] = 0
+
     engine = engine_from_config(settings, 'sqlalchemy.')
     DBSession.configure(bind=engine)
+
     config = Configurator(settings=settings)
     config.add_static_view('static', 'static', cache_max_age=3600)
+
     config.add_route('index', '/')
     config.add_route('js', '/paste/{id}.js')
     config.add_route('view', '/paste/{id}')
+
     config.scan()
     return config.make_wsgi_app()
 
